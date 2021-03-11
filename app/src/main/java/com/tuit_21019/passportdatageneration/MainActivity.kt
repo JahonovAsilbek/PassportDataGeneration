a8b153534804747d3c9a5791420d4fd048758f51
Olimjon <olimjonrustamov3477@gmail,com>
2021-03-11 22:13:02 +0500

    project started

---
+++ app/src/main/java/com/tuit_21019/passportdatageneration/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tuit_21019.passportdatageneration
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
