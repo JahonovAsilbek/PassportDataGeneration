0b3a8dd3ba45056ac16b4ae13767e810e2c4144e
JahonovAsilbek <asilbekjahonov2001@gmail.com>
2021-03-13 14:37:35 +0500

    search qoldi faqat

---
+++ app/src/main/java/com/tuit_21019/passportdatageneration/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.tuit_21019.passportdatageneration
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.tuit_21019.passportdatageneration.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // This is used to hide the status bar and make
+        // the splash screen as a full screen activity.
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+            WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        // we used the postDelayed(Runnable, time) method
+        // to send a message with a delayed time.
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000) // 3000 is the delayed time in milliseconds.
+    }
+}
