06fb3a638048806c86cfc239332e608e80e8caa1
JahonovAsilbek <asilbekjahonov2001@gmail.com>
2021-09-13 16:36:06 +0500

    edit/delete added

--- app/src/main/java/com/tuit_21019/passportdatageneration/MainActivity.kt
+++ app/src/main/java/com/tuit_21019/passportdatageneration/MainActivity.kt
@@ -1,17 +1,12 @@
 package com.tuit_21019.passportdatageneration
 
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import com.tuit_21019.passportdatageneration.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-    private var doublePressed = false
-    lateinit var handler: Handler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
